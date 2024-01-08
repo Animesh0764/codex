c56bf4025fcb92e0c63a8097484a0ae18496f06c
Animesh Singh <87444276+Animesh0764@users.noreply.github.com>
2024-01-08 16:07:15 +0530

    changed settings

--- codex/codex/settings.py
+++ codex/codex/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-0f@#8u%d4z6s86j8)-!2!b%ag2yui^qacjk2b_fyw!nk!v+)*x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
